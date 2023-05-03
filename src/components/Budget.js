05e340a3e2b13a42538f41f89904a85bfdfd77d0
brobinsoncld <80985791+rulootboxog@users.noreply.github.com>
2023-05-02 23:21:29 -0400

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,7 +4,7 @@ const Budget = () => {
     const { budget } = useContext(AppContext);
     return (
         <div className='alert alert-secondary'>
-            <span contenteditable>Budget: £{budget}</span>
+            <span contenteditable={true}>Budget: £{budget}</span>
         </div>
     );
 };
