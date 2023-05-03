21aea0e4c562254fca083999e3e0d01156c14538
brobinsoncld <80985791+rulootboxog@users.noreply.github.com>
2023-05-03 01:29:58 -0400

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
-import { totalExpenses } from ExpenseTotal;
+import { totalExpenses } from '../components/ExpenseTotal.js';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
