90bb2e8a7a4a4f5d91e45789ccc1ddde51b10a60
brobinsoncld <80985791+rulootboxog@users.noreply.github.com>
2023-05-03 02:07:03 -0400

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import { totalExpenses } from './ExpenseTotal';
-const { expenses } = useContext(AppContext);
+
+
+const Budget = () => {
+    const { expenses } = useContext(AppContext);
     totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
     }, 0);
-
-const Budget = () => {
     const { budget } = useContext(AppContext);
     return (
         <div className='alert alert-secondary'>
