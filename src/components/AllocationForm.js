73e6f229ac3b664a24fb070f05d6a33f13899045
brobinsoncld <80985791+rulootboxog@users.noreply.github.com>
2023-05-05 01:06:00 -0400

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -52,7 +52,7 @@ const AllocationForm = (props) => {
             }
 
         
-         
+         money = "$"
 
     };
 
@@ -98,7 +98,8 @@ const AllocationForm = (props) => {
                         <option value="₹" name="₹">₹</option>
                   </select>
                 
-                    <h3 defaultValue="$" style={{ marginLeft: '2rem' , size: 10}}>{money}</h3>               
+                                    
+                    <h3 style={{ marginLeft: '2rem' , size: 10}}>{money}</h3>               
                     <input
                         required='required'
                         type='number'
