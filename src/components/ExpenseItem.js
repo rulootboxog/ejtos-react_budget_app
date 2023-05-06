f563ea47580003443525a4e5f3b795fe09e25010
brobinsoncld <80985791+rulootboxog@users.noreply.github.com>
2023-05-06 19:41:32 -0400

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -42,7 +42,7 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>${props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)} style={{ borderRadius: 100, width: 150, height: 150 }}>+</button></td>
+        <td><button onClick={event=> increaseAllocation(props.name)} style={{ borderRadius: 100, width: 150, height: 150, color: 'green' }}>+</button></td>
         <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
